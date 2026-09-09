372b1861991fd31b0e4aaf1f60bd340df4ab18de
aestriex <caidenm@duck.com>
2026-09-08 18:53:02 -0700

    [feat] Scaffold Tauri 2 + React + TS app shell

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "system-ui, sans-serif",
+    }}>
+      <h1 style={{ fontSize: "1.5rem", fontWeight: 500 }}>
+        My Editor
+      </h1>
+    </div>
+  );
+}
+
+export default App;
